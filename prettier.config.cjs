e1690ccad61494cfadd1f46a85ad0eedcd9b3efe
Miika Nissi <miika@miikanissi.com>
2024-11-27 01:40:04 -0500

    build: update CI and linters

---
+++ prettier.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('prettier').Config} */
+
+const config = {
+  bracketSpacing: false,
+  printWidth: 88,
+  proseWrap: "always",
+  semi: true,
+  trailingComma: "es5",
+  xmlWhitespaceSensitivity: "preserve",
+};
+
+module.exports = config;
